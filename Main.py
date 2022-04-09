9689fbc9ed5e3e45f8090fa62f38c0421ab5abe2
Jasonic121 <54169592+Jasonic121@users.noreply.github.com>
2022-04-09 15:57:26 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
-######## Video Stream Object Detection Using Tensorflow-trained Classifier #########
+######## Webcam Object Detection Using Tensorflow-trained Classifier #########
 #
-# Author: Evan Juras (update by JanT)
-# Date: 10/27/19 (updated 12/4/2019)
+# Author: Evan Juras
+# Date: 10/27/19
 # Description: 
-# This program uses a TensorFlow Lite model to perform object detection on a live video stream.
-# It draws boxes and scores around the objects of interest in each frame from the
-# stream. To improve FPS, the webcam object runs in a separate thread from the main program.
-# This script will work with codecs supported by CV2 (e.g. MJPEG, RTSP, ...).
+# This program uses a TensorFlow Lite model to perform object detection on a live webcam
+# feed. It draws boxes and scores around the objects of interest in each frame from the
+# webcam. To improve FPS, the webcam object runs in a separate thread from the main program.
+# This script will work with either a Picamera or regular USB webcam.
 #
 # This code is based off the TensorFlow Lite image classification example at:
 # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/examples/python/label_image.py
@@ -26,10 +26,10 @@ import importlib.util
 # Define VideoStream class to handle streaming of video from webcam in separate processing thread
 # Source - Adrian Rosebrock, PyImageSearch: https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/
 class VideoStream:
-    """Camera object that controls video streaming"""
+    """Camera object that controls video streaming from the Picamera"""
     def __init__(self,resolution=(640,480),framerate=30):
         # Initialize the PiCamera and the camera image stream
-        self.stream = cv2.VideoCapture(STREAM_URL)
+        self.stream = cv2.VideoCapture(0)
         ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
         ret = self.stream.set(3,resolution[0])
         ret = self.stream.set(4,resolution[1])
@@ -69,8 +69,6 @@ class VideoStream:
 parser = argparse.ArgumentParser()
 parser.add_argument('--modeldir', help='Folder the .tflite file is located in',
                     required=True)
-parser.add_argument('--streamurl', help='The full URL of the video stream e.g. http://ipaddress:port/stream/video.mjpeg',
-                    required=True)
 parser.add_argument('--graph', help='Name of the .tflite file, if different than detect.tflite',
                     default='detect.tflite')
 parser.add_argument('--labels', help='Name of the labelmap file, if different than labelmap.txt',
@@ -85,7 +83,6 @@ parser.add_argument('--edgetpu', help='Use Coral Edge TPU Accelerator to speed u
 args = parser.parse_args()
 
 MODEL_NAME = args.modeldir
-STREAM_URL = args.streamurl
 GRAPH_NAME = args.graph
 LABELMAP_NAME = args.labels
 min_conf_threshold = float(args.threshold)
